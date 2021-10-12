6f1bae6139d83be43b56564b349cddcbf2d8f4f5
root <jhancock0@protonmail.com>
2021-10-12 18:44:43 +0000

    Added command line argument functionality. -h or --help prints a help message, --version prints the version, -i and -o allow the user to provide input and output files.

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -1,10 +1,44 @@
 #include <iostream>
 #include <string>
-int main()
+#include <vector>
+
+int main(int argc, char* argv[])
 {
-    
+    const std::vector<std::string> cmdLineArgs { argv, argv+argc };
     char in_char{'x'};
     std::string out_str{""};
+
+    for (int i{1}; i < cmdLineArgs.size(); i++)
+    {
+        if (cmdLineArgs[i] == "-h" || cmdLineArgs[i] == "--help")
+        {
+            std::cout << "The program takes an input and transliterates it. Letters are made to be upper case, numbers are replaced with their corresponding word (2 becomes TWO). All other characters are ignored. To continue, type an input, press enter, then press Ctrl+D." << std::endl;
+        }
+        else if (cmdLineArgs[i] == "--version")
+        {   
+            /* I have no idea how to get the version number out of github, so I'm doing it manually for now */
+            std::cout << "v0.2.0" << std::endl;
+        }
+        else if (cmdLineArgs[i] == "-i")
+        {
+            std::string input_file{cmdLineArgs[i+1]};
+            std::cout << "Input file: " << input_file << std::endl;
+            i++;
+        }
+        else if (cmdLineArgs[i] == "-o")
+        {
+            std::string output_file{cmdLineArgs[i+1]};
+            std::cout << "Output file: " << output_file << std::endl;
+            i++;
+        }
+        else
+        {
+            std::cout << "Error: "<< cmdLineArgs[i] << " is not a valid command!" << std::endl;    
+            return(0);
+        }
+        
+    }
+    
 
     while (std::cin >> in_char)
     {
@@ -19,7 +53,6 @@ int main()
             out_str = out_str + in_char;
             continue;
         }
-        std::cout << "number!" << std::endl;
         /* If the loop is still going, then the character is not a letter. We first check for numbers. */
         switch (in_char)
         {
